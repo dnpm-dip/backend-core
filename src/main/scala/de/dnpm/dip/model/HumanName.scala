1a7d602a87c8754d7483bc78bc8ed5c7b019af52
Paul-Christian Volkmer <volkmer_p@ukw.de>
2024-02-22 08:40:31 +0100

    Add other (optional) parts of a human name
    * Some names contain a middle name
    * A Prefix can be used for optional parts in front of other parts of the name
    * A Postfix can be used for optional parts at the end of the full name

--- src/main/scala/de/dnpm/dip/model/HumanName.scala
+++ src/main/scala/de/dnpm/dip/model/HumanName.scala
@@ -5,7 +5,10 @@ package de.dnpm.dip.model
 final case class HumanName
 (
   givenName: String,
+  middleName: Option[String],
   familyName: String,
-  title: Option[String]
+  title: Option[String],
+  prefix: Option[String],
+  postfix: Option[String],
 )
 
