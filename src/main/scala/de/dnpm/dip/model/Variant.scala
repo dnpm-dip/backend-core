a80573f086d525d39cb76cc7699992b119f7a9ab
Lucien Clin <lucien.clin@uni-tuebingen.de>
2025-07-03 14:07:59 +0200

    fix: Unified Chromosome value set to include chrMT, in accordance with MVH specs

--- src/main/scala/de/dnpm/dip/model/Variant.scala
+++ src/main/scala/de/dnpm/dip/model/Variant.scala
@@ -6,6 +6,11 @@ import de.dnpm.dip.coding.{
   CodedEnum,
   DefaultCodeSystem,
 }
+import play.api.libs.json.{
+  Json,
+  Format
+}
+
 
 
 trait BaseVariant
@@ -15,7 +20,6 @@ trait BaseVariant
   val patient: Reference[Patient]
   val localization: Option[Set[Coding[BaseVariant.Localization.Value]]]
 }
-
 
 object BaseVariant
 {
@@ -44,6 +48,39 @@ object BaseVariant
 }
 
 
+object Chromosome extends Enumeration
+{
+  val chr1,
+      chr2,
+      chr3,
+      chr4,
+      chr5,
+      chr6,
+      chr7,
+      chr8,
+      chr9,
+      chr10,
+      chr11,
+      chr12,
+      chr13,
+      chr14,
+      chr15,
+      chr16,
+      chr17,
+      chr18,
+      chr19,
+      chr20,
+      chr21,
+      chr22,
+      chrX,
+      chrY,
+      chrMT = Value
+
+  implicit val format: Format[Value] =
+    Json.formatEnum(this)
+}
+
+/*
 trait Chromosome
 {
   this: Enumeration => 
@@ -73,4 +110,4 @@ trait Chromosome
       chrX,
       chrY = Value
 }
-
+*/
